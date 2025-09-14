0559ce9b4f2c41b9de779af153b5a82914737f25
WK-Kz <molaskendall@gmail.com>
2025-09-14 13:49:21 -0700

    roa2_config_setup.sh : Fixing syntax to make more sense bash-like

--- roa2_config_setup.sh
+++ roa2_config_setup.sh
@@ -25,9 +25,9 @@ chmod 444 $GAME_USER_INI_PATH
 
 # Installation of Potato Mod
 PATH_TO_STEAM_LOCAL=$PATH_TO_STEAM_APPS/common/Rivals\ 2/Rivals2/Content/Paks
-POTATO_MOD_PATH="$PATH_TO_STEAM_LOCAL"/potato.zip
+POTATO_MOD_PATH=$PATH_TO_STEAM_LOCAL/potato.zip
 cd "$PATH_TO_STEAM_LOCAL"
-wget -O POTATO_MOD_PATH https://gamebanana.com/dl/1446017
-unzip POTATO_MOD_PATH
-rm POTATO_MOD_PATH
+wget -O "$POTATO_MOD_PATH" https://gamebanana.com/dl/1446017
+unzip "$POTATO_MOD_PATH"
+rm "$POTATO_MOD_PATH"
 
