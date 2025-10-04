b2015f37c93514ea4d5eddd7de0cdfeb961d3dc0
WK-Kz <molaskendall@gmail.com>
2025-10-04 05:21:14 -0700

    Remove potato mod from being installed from main optimization script

--- roa2_config_setup.bat
+++ roa2_config_setup.bat
@@ -106,22 +106,5 @@ REM Copy files from specified option to Steam Config
 xcopy /r %SELECTED_DIR% %PATH_TO_STEAM_CONFIG%
 attrib +r %PATH_TO_STEAM_CONFIG%\*
 
-
-:potato_mod_install
-set PATH_TO_STEAM_APPS=%programfiles(x86)%\Steam\steamapps
-
-@echo "Installation of Potato Mod"
-
-set PATH_TO_STEAM_LOCAL=%PATH_TO_STEAM_APPS%\common\Rivals 2\Rivals2\Content\Paks
-set POTATO_MOD_PATH=%PATH_TO_STEAM_LOCAL%
-set POTATO_FILE_NAME=%POTATO_MOD_PATH%\potato.zip
-curl -L "https://gamebanana.com/dl/1446017" -o "%POTATO_FILE_NAME%"
-pushd .
-cd %POTATO_MOD_PATH%
-tar -xf "%POTATO_FILE_NAME%"
-del "%POTATO_FILE_NAME%"
-popd
-
-
 :done
 @echo Exiting script.
